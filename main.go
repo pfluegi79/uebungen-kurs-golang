98efa08a2ed8e883e71454725fc67350343f6284
Marc Pfluger <pfluegi@bluewin.ch>
2024-04-27 14:37:24 +0000

    Lektion 18 Crashkurs Go Udemy

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hallo User!!")
+	foo()
+	fmt.Println("Foo ist beendet. Hier geht's weiter im Hauptteil")
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	bar()
+}
+
+func foo() {
+	fmt.Println("Hier sind wir in der Funktion Foo()")
+}
+
+func bar() {
+	fmt.Println("Hier sind wir in der Funktion Bar()")
 }
