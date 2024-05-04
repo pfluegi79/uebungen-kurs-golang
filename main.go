92f499e338fd1205f41f527ebefac70ae5939e8f
Marc Pfluger <pfluegi@bluewin.ch>
2024-05-04 17:42:46 +0000

    Lektion 28 Crashkurs Go Udemy Übung 1

--- main.go
+++ main.go
@@ -4,26 +4,13 @@ import (
 	"fmt"
 )
 
-var a int
-
-var b ganzzahl
-
-var c string
-
-type ganzzahl int
-
 func main() {
-	a = 23
-	fmt.Printf("a hat den Wert:  %v\n", a)
-	fmt.Printf("a hat den Typ:  %b\n", a)
-	fmt.Printf("a hat den Typ:  %T\n", a)
-	b = 42
-	fmt.Printf("a hat den Wert:  %v\n", b)
-	fmt.Printf("a hat den Typ:  %b\n", b)
-	fmt.Printf("a hat den Typ:  %T\n", b)
-	a = int(b)
-	fmt.Printf("a hat den Wert:  %v\n", a)
-	fmt.Printf("a hat den Typ:  %b\n", a)
-	fmt.Printf("a hat den Typ:  %T\n", a)
-	// c = "222"
+	a := 23
+	b := "Papa Schlumpf"
+	c := true
+
+	fmt.Println(a, b, c)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
