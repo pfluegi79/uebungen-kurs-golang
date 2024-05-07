260bc4b26a6ad2f58fb575d8f773e58e0c515856
Marc Pfluger <pfluegi@bluewin.ch>
2024-05-07 04:54:43 +0000

    Lektion 36 Crashkurs Go Udemy Übung 5

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 type ganzzahl int
 
 var x ganzzahl
+var y int
 
 func main() {
 	fmt.Printf("Der Wert von x ist: %v\n", x)
 	fmt.Printf("Der Type von x ist: %T\n", x)
 	x = 23
 	fmt.Printf("Der neue Wert von x ist: %v\n", x)
+	y = int(x)
+	fmt.Printf("Der Wert von y ist: %v\n", y)
+	fmt.Printf("Der Type von y ist: %T\n", y)
 }
